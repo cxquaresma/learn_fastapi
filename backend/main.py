8991d1b704dd9d8d056766664699faf07c8f3f1a
cxquaresma <57335050+cxquaresma@users.noreply.github.com>
2025-06-10 20:50:26 -0400

    query params

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,45 @@
-from enum import Enum
-
 from fastapi import FastAPI
-
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+# this is a query parameter, which goes after the question mark in the URL:
+# like /items/?skip=0&limit=10
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
+#making the paramater 'q' optional
+@app.get("/item/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
 
-    return {"model_name": model_name, "message": "Have some residuals"}
-    
+# type conversion
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
+
+# multiple path parameters
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+        user_id: int, item_id: str, q: str | None = None, short: bool = False
+    ):
+    item = {"owner_id": user_id, "item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
+
+# required query parameters - it will fail if 'needy' is not provided
+@app.get("/items/{item_id}")
+async def read_item_details(item_id: str, needy: str):
+    return {"item_id": item_id, "needy": needy}
